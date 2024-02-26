217be3df893f20ae0dfaecf4828face9e2b1d981
jekforger <mudzaqimaulana@gmail.com>
2024-02-26 22:39:32 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# MDC - Pertemuan 3
+
+## Proyek 4
+
+- Nama:Adrian Eka Saputra
+- Kelas : 2B
+- NIM : 221511036
+
+## Screenshot
+
+### Pertemuan 3
+
+- ![Screenshot App (C)](https://github.com/adrianeka/Codelab/blob/main/SS1.jpg?raw=true)
+- ![Screenshot App (F)](https://github.com/adrianeka/Codelab/blob/main/SS2.jpg?raw=true)
+- ![Screenshot App (F)](https://github.com/adrianeka/Codelab/blob/main/SS3.jpg?raw=true)
---
+++ lib/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kShrinePink50 = Color.fromARGB(0, 31, 175, 86);
+const kShrinePink100 = Color.fromARGB(0, 71, 152, 71);
+const kShrinePink300 = Color(0xB6C4B6);
+const kShrinePink400 = Color(0xEEF0E5);
+
+const kShrineBrown900 = Color(0x88AB8E);
+
+const kShrineErrorRed = Color(0xAFC8AD);
+
+const kShrineSurfaceWhite = Color.fromARGB(0, 221, 251, 255);
+const kShrineBackgroundWhite = Colors.white;
+const kShrinePurple = Color(0xF2F1EB);
+const kPrimaryColor = Color.fromARGB(255, 73, 253, 199);
+const kDarkPrimaryColor = Color.fromARGB(255, 59, 253, 83);
